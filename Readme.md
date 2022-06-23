1d82abdf30bf42e822088059ea8e1930c035841c
CoveredTechSis <aishaolalekan@gmail.com>
2022-06-23 22:50:51 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+#Javascript-Task
+
+A practical of a Javascript variable introduction.It is made with Javascript and linked with HTML.
---
+++ name.js
@@ -0,0 +1,13 @@
+let name= "Olalekan Aishat Bisola"
+let height= "5.2ft"
+let country="Nigeria"
+console.log("name" +"height" + "country")
+alert("Name: Olalekan Aishat Bisola");
+alert("Height: 5.2ft")
+alert("Country: Nigeria")
+
+document.write("Name: Olalekan Aishat Bisola")
+document.write("Height: 5.2ft" )
+document.write("Country: Nigeria" )
+
+
